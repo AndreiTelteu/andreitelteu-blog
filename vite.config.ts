81bb8c8b3e20f968f97e6e1724d8c13a28294540
Andrei Telteu <andrei.telteu@touch-media.ro>
2023-01-08 20:10:15 +0200

    Installed solid-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
+import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
     plugins: [
+        devtools({
+            autoname: true,
+        }),
         {
             ...(await import('@mdx-js/rollup')).default({
                 jsx: true,
