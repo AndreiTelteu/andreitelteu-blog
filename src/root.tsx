cec46cd46f243d993df6427b22e0def0f3b4d1a1
Andrei Telteu <andrei.telteu@touch-media.ro>
2023-01-15 01:12:23 +0200

    Installed dejavu font

--- src/root.tsx
+++ src/root.tsx
@@ -1,15 +1,17 @@
 // @refresh reload
-import { Body, Head, Html, Meta, Scripts, Title } from 'solid-start';
+import { Body, Head, Html, Meta, Scripts, Style, Title } from 'solid-start';
 import './root.css';
+import inline from './critical.css?inline';
 import Router from './router';
 
 export default function Root() {
     return (
         <Html lang="en">
             <Head>
-                <Title>SolidStart - With MDX</Title>
+                <Title>AndreiTelteu</Title>
                 <Meta charset="utf-8" />
                 <Meta name="viewport" content="width=device-width, initial-scale=1" />
+                <Style>{inline}</Style>
             </Head>
             <Body>
                 <Router />
