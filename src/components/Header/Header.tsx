a4ee74c9947247699978f972c57d34c7516b0dbb
Andrei Telteu <andrei.telteu@touch-media.ro>
2023-01-20 19:50:24 +0200

    added categories

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,14 @@
 import { A } from '@solidjs/router';
+import { For } from 'solid-js';
 import { Style } from 'solid-start';
 import CriticalHeader from './Header.css?inline';
 
 export default function Header() {
+    const categories = [
+        { slug: 'opensource', name: 'OpenSource' },
+        { slug: 'projects', name: 'Projects' },
+        { slug: 'blog', name: 'Blog' },
+    ];
     return (
         <>
             <Style>{CriticalHeader}</Style>
@@ -11,6 +17,15 @@ export default function Header() {
                 <div class="header-container container">
                     <div class="header-logo">
                         <A href="/">Andrei Telteu</A>
+                    </div>
+                    <div class="header-menu">
+                        <For each={categories}>
+                            {(cat) => (
+                                <A href={`/${cat.slug}`} class="header-menu-link">
+                                    {cat.name}
+                                </A>
+                            )}
+                        </For>
                     </div>
                 </div>
             </header>
